91d975b340a8b82867eae3b14ac6d5b0bdeed6fb
Ajay Sharma <ajaysharma@Ajays-MacBook-Pro.local>
2023-12-08 22:02:43 +0530

    server setup

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("./config/db.config");
+
+const app = express();
+const PORT = process.env.PORT || 4321;
+
+if (process.env.NODE_ENV === "development") {
+  require("dotenv").config();
+}
+
+connectDB();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: "true" }));
+
+app.use("/api/auth", require("./routes/auth.routes"));
+
+app.listen(PORT, (error) => {
+  if (error) {
+    console.log(error?.message);
+  }
+  console.log(`Server Running on Port ${PORT}`);
+});
