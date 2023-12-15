eac4720d866e478909259095e32d12010a26c80b
ajaysharma12799 <ajaysharma12799@gmail.com>
2023-12-16 02:05:21 +0530

    Private and Public Route Basic Setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,8 @@ import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import WriteArticle from "./pages/WriteArticle/WriteArticle";
 import { Toaster } from "react-hot-toast";
+import PrivateRoute from "./components/PrivateRoute/PrivateRoute";
+import PublicRoute from "./components/PrivateRoute/PublicRoute";
 
 const App = () => {
   return (
@@ -12,10 +14,38 @@ const App = () => {
       <BrowserRouter>
         <Toaster />
         <Routes>
-          <Route path="/" element={<Register />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/write-article" element={<WriteArticle />} />
+          <Route
+            path="/"
+            element={
+              <PublicRoute>
+                <Register />
+              </PublicRoute>
+            }
+          />
+          <Route
+            path="/login"
+            element={
+              <PublicRoute>
+                <Login />
+              </PublicRoute>
+            }
+          />
+          <Route
+            path="/dashboard"
+            element={
+              <PrivateRoute>
+                <Dashboard />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/write-article"
+            element={
+              <PrivateRoute>
+                <WriteArticle />
+              </PrivateRoute>
+            }
+          />
         </Routes>
       </BrowserRouter>
     </React.Fragment>
