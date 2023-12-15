7a6e0f7464aff0f395e21da69ab80803a16522ee
Ajay Sharma <ajaysharma@Ajays-MacBook-Pro.local>
2023-12-15 23:44:19 +0530

    Routing Setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,11 +4,13 @@ import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import WriteArticle from "./pages/WriteArticle/WriteArticle";
+import { Toaster } from "react-hot-toast";
 
 const App = () => {
   return (
     <React.Fragment>
       <BrowserRouter>
+        <Toaster />
         <Routes>
           <Route path="/" element={<Register />} />
           <Route path="/login" element={<Login />} />
