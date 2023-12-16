8de664a164f7a0553da31f0a91bf1377ea6bcc60
ajaysharma12799 <ajaysharma12799@gmail.com>
2023-12-17 00:19:35 +0530

    added article reducer

--- client/src/redux/Store.jsx
+++ client/src/redux/Store.jsx
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import AuthReducer from "./features/auth/auth.slice";
+import ArticleReducer from "./features/post/post.slice";
 
 const Store = configureStore({
   reducer: {
     auth: AuthReducer,
+    articles: ArticleReducer,
   },
 });
 
