7ebfe25eb9eabba391266dafd46aae16d0b8485b
ajaysharma12799 <ajaysharma12799@gmail.com>
2023-12-16 14:32:36 +0530

    Post Route Setup

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const connectDB = require("./config/db.config");
+const cookieParser = require("cookie-parser");
 
 const app = express();
 const PORT = process.env.PORT || 4321;
@@ -11,10 +12,12 @@ if (process.env.NODE_ENV === "development") {
 
 connectDB();
 app.use(cors());
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: "true" }));
 
 app.use("/api/auth", require("./routes/auth.routes"));
+app.use("/api/post", require("./routes/post.routes"));
 
 app.listen(PORT, (error) => {
   if (error) {
