2abdeebcebd1dadf097fde05a307fa5f15616132
ajaysharma12799 <ajaysharma12799@gmail.com>
2023-12-17 02:07:18 +0530

    added Blog-Categories, Image Max-Width & Max-Height

---
+++ client/src/utils/CONSTANT.js
@@ -0,0 +1,14 @@
+export const BLOG_CATEGORIES = [
+  "Wellness",
+  "Travel",
+  "Tech",
+  "Fashion",
+  "Food",
+  "Finance",
+  "Books",
+  "Fitness",
+  "Uncategorized",
+];
+
+export const IMAGE_MAX_WIDTH = 512;
+export const IMAGE_MAX_HEIGHT = 512;
