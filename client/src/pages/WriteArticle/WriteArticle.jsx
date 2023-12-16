5cf2d281c84789a3d8c8a41158da162a3e688263
ajaysharma12799 <ajaysharma12799@gmail.com>
2023-12-17 02:10:04 +0530

    added Image-Dimension-Condition and CreateArticle Feature Implemented

--- client/src/pages/WriteArticle/WriteArticle.jsx
+++ client/src/pages/WriteArticle/WriteArticle.jsx
@@ -4,8 +4,15 @@ import { useFormik } from "formik";
 import { useState } from "react";
 import PreviewArticle from "./PreviewArticle";
 import ArticleForm from "./ArticleForm";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { createArticle } from "../../redux/features/post/post.slice";
+import toast from "react-hot-toast";
+import { IMAGE_MAX_WIDTH, IMAGE_MAX_HEIGHT } from "../../utils/CONSTANT";
 
 const WriteArticle = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
   const [image, setImage] = useState("");
   const formik = useFormik({
     initialValues: {
@@ -13,12 +20,32 @@ const WriteArticle = () => {
       title: "",
       excerpt: "",
       content: "",
-      category: "",
+      category: "Uncategorized",
     },
     onSubmit: (values, { resetForm }) => {
-      console.log(values);
-      resetForm();
-      setImage("");
+      const articleFormData = new FormData();
+      articleFormData.append("title", values.title);
+      articleFormData.append("excerpt", values.excerpt);
+      articleFormData.append("content", values.content);
+      articleFormData.append("category", values.category);
+      articleFormData.append("post_image", values.image);
+
+      // Image Dimension Condition
+      const selectedImage = new Image();
+      selectedImage.src = URL.createObjectURL(values.image);
+
+      selectedImage.onload = () => {
+        if (
+          selectedImage.width > IMAGE_MAX_WIDTH &&
+          selectedImage.height > IMAGE_MAX_HEIGHT
+        ) {
+          toast.error("Please Select Image of 512X512 Size");
+        } else {
+          dispatch(
+            createArticle({ articleFormData, navigate, resetForm, setImage })
+          );
+        }
+      };
     },
   });
   return (
