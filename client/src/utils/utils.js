2cdcf7a12ab8b1f06edfc501c941ae1f9d9fd9f0
ajaysharma12799 <ajaysharma12799@gmail.com>
2023-12-17 15:41:27 +0530

    Fixed formatDate date issue

--- client/src/utils/utils.js
+++ client/src/utils/utils.js
@@ -1,13 +1,8 @@
 import { sha256 } from "js-sha256";
 
 const formatDate = (date) => {
-  const d = new Date(date);
-
-  const day = d.getDay();
-  const month = d.getMonth() + 1;
-  const year = d.getFullYear();
-
-  return `${day}/${month}/${year}`;
+  const d = new Date(date).toLocaleDateString();
+  return d;
 };
 
 const generateGravatar = (currentUser) => {
