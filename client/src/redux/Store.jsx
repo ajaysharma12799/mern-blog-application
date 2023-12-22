e68fc4e6b729af3ef3a4c1b4d985962dff589d0f
ajaysharma12799 <ajaysharma12799@gmail.com>
2023-12-23 01:48:50 +0530

    added Comment Reducer

--- client/src/redux/Store.jsx
+++ client/src/redux/Store.jsx
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import AuthReducer from "./features/auth/auth.slice";
 import ArticleReducer from "./features/post/post.slice";
+import CommentReducer from "./features/comments/comment.slice";
 
 const Store = configureStore({
   reducer: {
     auth: AuthReducer,
     articles: ArticleReducer,
+    comments: CommentReducer,
   },
 });
 
