d3d25eea3ae841863be85c23438984feedfbce93
ajaysharma12799 <ajaysharma12799@gmail.com>
2023-12-22 23:50:12 +0530

    Added Comment Card

--- client/src/pages/ArticleDetail/Comment.jsx
+++ client/src/pages/ArticleDetail/Comment.jsx
@@ -1,10 +1,17 @@
-import { Box } from "@mui/material";
+import { Box, Divider } from "@mui/material";
 import CommentForm from "./CommentForm";
+import CommentCard from "../../components/Card/CommentCard";
 
 const Comment = () => {
   return (
     <Box my={2}>
+      <Divider>Comments</Divider>
       <CommentForm />
+      <Box my={2}>
+        {[1, 2, 3, 4, 5].map((_, idx) => {
+          return <CommentCard key={idx} />;
+        })}
+      </Box>
     </Box>
   );
 };
